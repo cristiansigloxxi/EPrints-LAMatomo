8e19af0a6efa80667d0808f156fd6faaa812f0bc
cristiansigloxxi <37702346+cristiansigloxxi@users.noreply.github.com>
2020-03-11 11:15:59 -0300

    Update LAMatomo.pl

--- lib/defaultcfg/cfg.d/LAMatomo.pl
+++ lib/defaultcfg/cfg.d/LAMatomo.pl
@@ -28,29 +28,28 @@ require LWP::ConnCache;
 # CONFIG START  #
 ################
 
-# Modify the following URL to the Matomo tracker location
+# Matomo tracker location
 $c->{LAMatomo}->{tracker} = "http://matomo.lareferencia.info/matomo.php";
 
 # Enter the LA Referencia Matomo Site ID
 $c->{LAMatomo}->{siteID} = "47";
 
-# Enter the matomo token_auth
+# Enter the Matomo token_auth
 $c->{LAMatomo}->{token_auth} = "4351b5525feb89c92f54b94693b5a8f3";
 
 # Specify the number of bytes, 1,2 or 3, for IP Anonymization (empty for no IP Anonymization)
 $c->{LAMatomo}->{noOfBytes} = "";
 
-# Identifier in the Directory of Open Access Repositories (OpenDOAR)
+# Enter the identifier in the Directory of Open Access Repositories (OpenDOAR)
 $c->{LAMatomo}->{repositoryID} = "";
 
-# ISO Country Code
+# Enter the ISO Country Code (Alpha-2)
 $c->{LAMatomo}->{countryID} = "";
 
 # Other Config Parameters
 $c->{LAMatomo}->{ua} = LWP::UserAgent->new(conn_cache => LWP::ConnCache->new,);
 
 $c->{plugins}->{"Event::LAMatomo"}->{params}->{disable} = 0;
-
 
 ################
 # CONFIG END  #
